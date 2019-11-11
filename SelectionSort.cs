a2469bbc16747c20da849c7372ea793cc6c52e92
Gary Mayfield <gary.mayfield@hotmail.co.uk>
2019-11-11 18:34:55 +0000

    Added c# selectionsort

---
+++ SelectionSort.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace SelectionSort
+{
+    class SelectionSort
+    {
+        static void Main()
+        {
+            Random random = new Random();
+
+            var arr = Enumerable
+                .Repeat(0, 10)
+                .Select(x => random.Next(1,25))
+                .ToArray();
+
+            Console.WriteLine("Unsorted array: ");
+            PrintArray(arr);
+            SelectionSort(arr);
+            Console.WriteLine("Sorted array: ");
+            PrintArray(arr);
+        }
+
+        static void SelectionSort(int[] data)
+        {
+            for (int i = 0; i < data.Length - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < data.Length; j++)
+                {
+                    if (data[j] < data[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                var temp = data[minIndex];
+                data[minIndex] = data[i];
+                data[i] = temp;
+            }
+
+        }
+
+        static void PrintArray(int[] data)
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.WriteLine(data[i]);
+            }
+        }
+    }
+}
